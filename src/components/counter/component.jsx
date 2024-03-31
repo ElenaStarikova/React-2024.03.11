2555223f8dca1bfa780b75ccd154b7d3a4a5e1c1
unknown <elena.g.starikova@yandex.ru>
2024-04-01 01:12:50 +0700

    useCount

--- src/components/counter/component.jsx
+++ src/components/counter/component.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react';
+
+import { useCount } from '../../hooks/use-count.js';
 
 export const Counter = () => {
-    const[amount, setAmount]  = useState(0)
+    const[amount, increment, decrement]  = useCount(0)
 
     return(
         <div>
-            <button onClick = {() => setAmount(amount - 1)} disabled = {amount === 0}>
+            <button onClick = {() => decrement()} disabled = {amount === 0}>
                 -
             </button>
-            <button onClick = {() => setAmount(amount + 1)} disabled = {amount === 5}>
+            {amount}
+            <button onClick = {() => increment()} disabled = {amount === 5}>
                 +
             </button>
         </div>
